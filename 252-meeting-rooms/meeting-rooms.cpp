a48a4c0f4456061da94d8f8b60c4275db75d239c
devanshubisht <devanshubisht.2000@gmail.com>
2023-07-19 17:26:32 +0800

    Time: 19 ms (67.98%) | Memory: 11.7 MB (51.57%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.cpp
+++ 252-meeting-rooms/meeting-rooms.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if (intervals.empty()) {
+            return true;
+        }
         sort(intervals.begin(), intervals.end());
         for (int i = 1 ; i < intervals.size(); i++) {
             if (intervals[i-1][1]>intervals[i][0]) {
