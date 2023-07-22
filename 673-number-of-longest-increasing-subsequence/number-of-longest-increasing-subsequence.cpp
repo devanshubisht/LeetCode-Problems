35ff1124b9dfeca652bcf181bbf6e2bced25815c
devanshubisht <devanshubisht.2000@gmail.com>
2023-07-22 16:59:52 +0800

    Time: 202 ms (67.75%) | Memory: 13.1 MB (66.29%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
+    
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size(), res = 0, max_len = 0;
-        vector<pair<int,int>> dp(n,{1,1});            //dp[i]: {length, number of LIS which ends with nums[i]}
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j <i ; j++){
-                if(nums[i] > nums[j]){
-                    if(dp[i].first == dp[j].first + 1)dp[i].second += dp[j].second;
-                    if(dp[i].first < dp[j].first + 1)dp[i] = {dp[j].first + 1, dp[j].second};
+        const int n = nums.size();
+        vector<int> lis(n,1);  // stores length of longest sequence till i-th position
+        vector<int> count(n,1);  // stores count of longest sequence of length lis[i]
+        int maxLen = 1;  // maximum length of lis
+		
+		// O(N^2) DP Solution
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    if(lis[j] + 1 > lis[i]){ // strictly increasing
+                        lis[i] = lis[j] + 1;
+                        count[i] = count[j];
+                    } 
+					 // this means there are more subsequences of same length ending at length lis[i]
+					else if(lis[j]+1 == lis[i]){ 
+                        count[i] += count[j];
+                    }
                 }
             }
-            if(max_len == dp[i].first)res += dp[i].second;
-            if(max_len < dp[i].first){
-                max_len = dp[i].first;
-                res = dp[i].second;
-            }
+            maxLen = max(maxLen,lis[i]);
         }
-        return res;
+        
+        int numOfLIS = 0;
+        // count all the subseq of length maxLen
+        for(int i=0;i<n;i++){
+            if(lis[i]==maxLen)
+                numOfLIS += count[i];
+        }
+            
+        return numOfLIS;
     }
-
 };
