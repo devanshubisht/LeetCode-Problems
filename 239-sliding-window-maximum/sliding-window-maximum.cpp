25b3764fe29596b17b9f1f6a5b913833811989bf
devanshubisht <devanshubisht.2000@gmail.com>
2023-08-17 21:33:25 +0800

    Time: 213 ms (82.47%) | Memory: 134.7 MB (66.08%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -32,3 +32,10 @@ public:
 // -> move by 1.
 // ->  if the right value is greater than the max then that is the value.. if smaller than max
 // if the left was the max value, find new max value.
+
+// correct approach -> move along. add to queue for the first k. the indeces are added to the queue but only if the nums[i] is bigger than the front. keep popping. 
+// so the queue contains the largest numbers in those indices. do for the first window first.
+
+//continue to the next index. if teh first index is diff is away from the allowed then pop it.
+// -> deque.
+// -> dq.back() [get the irest index]. pop_back t remove tyhe first one.
