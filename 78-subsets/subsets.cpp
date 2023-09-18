98292153e0b757a8c6df782b1a318cda5d5a65ab
devanshubisht <devanshubisht.2000@gmail.com>
2023-09-19 03:26:39 +0800

    Time: 4 ms (39.44%) | Memory: 7.6 MB (49.58%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        dfs(nums, 0, temp);
+        return result;
+    }
+
+    void dfs(vector<int>& nums, int i, vector<int>& res) {
+        result.push_back(res);
+        for (int j = i; j < nums.size(); j++) {
+            res.push_back(nums[j]);
+            dfs(nums, j+1, res);
+            res.pop_back();
+        }
+    }
+
+};
