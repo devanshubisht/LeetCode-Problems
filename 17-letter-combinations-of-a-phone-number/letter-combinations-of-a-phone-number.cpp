10df73b0ac6624365195d4b891fc67ed6ed8ff6b
devanshubisht <devanshubisht.2000@gmail.com>
2023-09-21 03:12:58 +0800

    Time: 0 ms (100.00%) | Memory: 7.1 MB (6.85%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     vector<string> result;
-    unordered_map<char, string> hm;
-
+    
     vector<string> letterCombinations(string digits) {
-        if(digits.empty()){
+        if (digits.empty()) {
             return {};
         }
-        hm['2'] = "abc";
-        hm['3'] = "def";
-        hm['4'] = "ghi";
-        hm['5'] = "jkl";
-        hm['6'] = "mno";
-        hm['7'] = "pqrs";
-        hm['8'] = "tuv";
-        hm['9'] = "wxyz";
-        string temp;
-        dfs(temp, 0, digits);
-        return result;
         
+        vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        string temp;
+        dfs(temp, 0, pad, digits);
+        
+        return result;
     }
 
-    void dfs(string& temp, int i,  string digits) {
+    void dfs(string& temp, int i, const vector<string>& pad, const string& digits) {
         if (i == digits.size()) {
             result.push_back(temp);
             return;
         }
-    
-        for(auto val: hm[digits[i]]) {
-            temp.push_back(val);
-            dfs(temp, i + 1,   digits);
+        
+        int digit = digits[i] - '0';
+        const string& characters = pad[digit];
+        
+        for (char ch : characters) {
+            temp.push_back(ch);
+            dfs(temp, i + 1, pad, digits);
             temp.pop_back();
         }
     }
