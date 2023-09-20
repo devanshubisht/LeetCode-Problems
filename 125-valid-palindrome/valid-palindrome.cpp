df624eda910d13903d1a37f5eae46dbfd09c55b4
devanshubisht <devanshubisht.2000@gmail.com>
2023-09-21 01:38:44 +0800

    Time: 8 ms (33.87%) | Memory: 7.7 MB (21.16%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        string newS = "";
+        while (i < j) {
+            if (!isalnum(s[i])) { 
+                i++;
+                continue; 
+            }
+            if (!isalnum(s[j])) {
+                j--;
+                continue;
+            }
+            if (tolower(s[i]) != tolower (s[j])) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
