1c32fcaf37511a022e63abb04ffab5973c768082
devanshubisht <devanshubisht.2000@gmail.com>
2023-09-21 02:03:52 +0800

    Time: 123 ms (32.90%) | Memory: 80.3 MB (25.65%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     vector<vector<string>> result;
-
     vector<vector<string>> partition(string s) {
         vector<string> currentList;
         dfs(s, currentList, 0);
         return result;
+        
     }
 
-    void dfs(string &s, vector<string>& tempArr, int index) {
+    void dfs(string s, vector<string>& tempArr, int index) {
         if (index == s.size()) {
             result.push_back(tempArr);
-            return; // Add this return statement
+            return;
         }
 
-        for (int i = index; i < s.size(); i++) {
-            if (isPalindrome(s, index, i)) {
-                tempArr.push_back(s.substr(index, i - index + 1));
-                dfs(s, tempArr, i + 1);
+        for (int i = index ; i < s.size() ; i ++) {
+            string temp = s.substr(index, i - index + 1);
+            if (isPalindrome(temp)) {
+                tempArr.push_back(temp);
+                dfs(s, tempArr, i+1);
                 tempArr.pop_back();
             }
         }
     }
 
-    bool isPalindrome(string &s, int low, int high) {
-        while (low < high) {
-            if (s[low++] != s[high--]) return false;
-        }
+    bool isPalindrome(string s) {
+        int l = 0,r = s.size()-1;
+        while(l<r){
+            while(l<r && !isalnum(s[l])) l++;
+            while(l<r && !isalnum(s[r])) r--;
+            if(tolower(s[l])!=tolower(s[r])) return false;
+            l++;
+            r--;
+            }
         return true;
     }
 };
