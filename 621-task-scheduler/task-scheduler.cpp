f212d68507ed3e9d6c91a6c62ca5bdb244682ff1
devanshubisht <devanshubisht.2000@gmail.com>
2023-09-27 03:09:26 +0800

    Time: 108 ms (14.59%) | Memory: 49.3 MB (8.69%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> hm;
-        for (int i = 0; i < tasks.size(); i++) {
+        priority_queue<pair<int,char>> pq;
+        unordered_map<char,int> hm;
+        for(int i = 0; i < tasks.size(); i++) {
             hm[tasks[i]]++;
         }
-        priority_queue<int> pq;
-        for (auto a : hm) {
-            pq.push(a.second);  
-        }
-        int time = 0;
-        while (1) {
-            vector<int> remain;
-            int cycle = n + 1;
-            while (cycle and !pq.empty()) {
-                int first = pq.top();
-                pq.pop();
-                if (first > 1) {
-                    remain.push_back(first -1);
-                }
-                cycle--;
-                time++;
-            }
 
-            for (int count:remain) {
-                pq.push(count);
-            }
-            if (pq.empty()) {
+        for (auto num : hm) {
+            pq.push({num.second, num.first});
+        }
+        int result = 0;
+
+        while (pq.size() > 0) {
+            if (pq.top().first == 1) {
+                result += pq.size();
                 break;
             }
-            time += cycle;
+            vector<pair<int,char>> temp;
+            for (int i = 0; i < n + 1; i++) {
+                if (pq.size() > 0) {
+                    pair<int,char> fir = pq.top();
+                    pq.pop();
+                    temp.push_back(fir);
+                }
+                result++;
+            }
+            for(auto x : temp) {
+                if(x.first>1) pq.push({x.first-1,x.second});
+            }
+
         }
-        return time;
+        return result;
     }
 };
-
-// Aproach. Pair and then put into a heap. pop out of the heap and add tgth. 
-//  take out 
