da523ecb2d5f3230162f6685e45c860eeb132754
devanshubisht <devanshubisht.2000@gmail.com>
2023-10-07 23:09:18 +0800

    Time: 11 ms (75.59%) | Memory: 14.3 MB (17.23%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
+
     struct compare {
-        bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        bool operator()( pair<int,int>& a, pair<int,int>& b) {
             return a.second < b.second;
         }
     };
-    
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> map;
-        for (int num : nums) {
-            map[num]++;
-        }
         
+        for (int i = 0 ; i < nums.size(); i++) {
+            map[nums[i]]++;
+        }
         vector<pair<int, int>> elements(map.begin(), map.end());
-        priority_queue<pair<int, int>, vector<pair<int, int>>, compare> maxHeap(elements.begin(), elements.end());
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, compare > pq(elements.begin(), elements.end());
 
         vector<int> res;
-        for (int i = 0; i < k; i++) {
-            pair<int, int> a = maxHeap.top();
-            maxHeap.pop();
+        for (int i = 0 ; i < k ; i++) {
+            pair<int,int> a = pq.top();
+            pq.pop();
             res.push_back(a.first);
         }
         return res;
+
     }
 };
