8e65e2b3561c69715f1d02abeaa415031f34d4c1
devanshubisht <devanshubisht.2000@gmail.com>
2023-10-10 14:34:29 +0800

    Time: 465 ms (21.48%) | Memory: 149.6 MB (17.04%) - LeetSync

--- 2207-sequentially-ordinal-rank-tracker/sequentially-ordinal-rank-tracker.cpp
+++ 2207-sequentially-ordinal-rank-tracker/sequentially-ordinal-rank-tracker.cpp
@@ -1,27 +1,34 @@
 class SORTracker {
 public:
-    std::set<pair<int,string>> s;
-    set<pair<int,string>>::iterator it;
-    SORTracker() {
-    }
-    
-    void add(string name, int score) {
-        auto it1 = s.insert({-score,name}).first;
-        if (s.size() == 1) {
-            it = s.begin();
-            return;
-        } 
-        if (*it1  < *it) {
-            it--;
+    struct cmp{
+        bool operator() (pair<string, int> a, pair<string, int> b) const{
+            if(a.second == b.second) return a.first < b.first;
+            else return a.second > b.second;
         }
+    };
+    
+    set<pair<string, int>, cmp> s;
+    set<pair<string, int>>::iterator it;
+    
+    SORTracker() {
+        s.insert({"", INT_MIN});
+        it = s.begin();
+    }
+
+    void add(string name, int score) {
+        auto [_name, _score] = *it;
+        if((_score < score) || (score == _score && _name > name)){
+            s.insert({name, score});
+            --it;
+        } 
+        else s.insert({name, score});
     }
     
     string get() {
-        const string& val = it->second;
-        it++;
-        return val;
+        string ans = it++->first;
+        return ans;
     }
-};
+};;
 
 /**
  * Your SORTracker object will be instantiated and called as such:
