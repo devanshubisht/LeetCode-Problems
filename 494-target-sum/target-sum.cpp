7ba2060a13abc84f3dba4431b1e4e42bed2fae48
devanshubisht <devanshubisht.2000@gmail.com>
2023-10-14 17:55:11 +0800

    Time: 316 ms (32.80%) | Memory: 63 MB (8.01%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return backtrack(nums, target, 0, 0);
+    }
+private:
+    // {(index, total) -> # of ways}
+    map<pair<int, int>, int> dp;
+    
+    int backtrack(vector<int>& nums, int target, int i, int total) {
+        if (i == nums.size()) {
+            return total == target ? 1 : 0;
+        }
+        if (dp.find({i, total}) != dp.end()) {
+            return dp[{i, total}];
+        }
+        
+        dp[{i, total}] = backtrack(nums, target, i + 1, total + nums[i])
+                       + backtrack(nums, target, i + 1, total - nums[i]);
+        
+        return dp[{i, total}];
+    }
+};
