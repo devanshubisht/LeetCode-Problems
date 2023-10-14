83bed0ac7f9bd5e8465fcf2f8c3bf221be2186c6
devanshubisht <devanshubisht.2000@gmail.com>
2023-10-14 16:52:46 +0800

    Time: 35 ms (31.60%) | Memory: 18.7 MB (35.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    
-    vector<vector<int>> dp;
     int change(int amount, vector<int>& coins) {
-        dp.resize(coins.size() + 1,vector<int>(amount + 1,0));
-        for (int i = 0 ; i < dp.size(); i ++) {
+        vector<vector<int>> dp (coins.size() + 1, vector<int>(amount + 1, 0));
+        for (int i = 0 ; i <= amount ; i++) {
+            dp[coins.size()][i] = 0;
+        }
+        for (int i = 0 ; i < dp.size(); i++) {
             dp[i][0] = 1;
         }
-
-        for (int i = 1; i < dp.size();i++) {
-            for (int j = 1; j < dp[0].size(); j++) {
-                dp[i][j] = dp[i-1][j];
-                if (j - coins[i-1] >= 0) {
-                    dp[i][j] += dp[i][j-coins[i-1]];
+        for (int i = coins.size() - 1 ; i >= 0 ; i--) {
+            for (int j = 1 ; j <= amount ; j++) {
+                if (coins[i] <= j) {
+                    dp[i][j] += dp[i][j - coins[i]] + dp[i+1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
                 }
+
             }
         }
-        return dp[coins.size()][amount];
-
+        return dp[0][amount];
     }
 
 };
