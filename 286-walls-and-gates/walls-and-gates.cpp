38d5abcf09bafc4430b328791b7ac1e1f3f4bd02
devanshubisht <devanshubisht.2000@gmail.com>
2023-10-14 21:47:37 +0800

    Time: 408 ms (5.19%) | Memory: 62.6 MB (5.69%) - LeetSync

---
+++ 286-walls-and-gates/walls-and-gates.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        queue<pair<int,int>> q;
+        for (int i = 0 ; i < rooms.size() ; i++) {
+            for (int j = 0 ; j < rooms[0].size() ; j++) {
+                if (rooms[i][j] == 0) {
+                    q.push({i,j});
+                }
+            }
+        }
+        vector<int> dir = {-1,0,1,0,-1};
+        int dist = 0;
+        while (!q.empty()) {
+            int s = q.size();
+            for (int i = 0 ; i < s; i++) {
+                pair<int,int> val = q.front();
+                q.pop();
+                rooms[val.first][val.second] = min(dist, rooms[val.first][val.second]);
+                for (int j = 0 ; j < 4 ; j ++) {
+                    int nextX = val.first + dir[j];
+                    int nextY = val.second + dir[j+1];
+                    if (nextX < 0 || nextX >= rooms.size() || 
+                        nextY < 0 || nextY >= rooms[0].size() ||
+                        rooms[nextX][nextY] == -1 || rooms[nextX][nextY] != INT_MAX) {
+                            continue;
+                        }
+                    q.push({nextX,nextY});
+                }
+            }
+            dist++;
+        }
+    }
+};
