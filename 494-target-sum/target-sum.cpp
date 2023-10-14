e7756422189f4eff91d2530380b750df7c764325
devanshubisht <devanshubisht.2000@gmail.com>
2023-10-14 17:56:40 +0800

    Time: 326 ms (31.93%) | Memory: 90.5 MB (5.03%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,26 @@
-class Solution {
+class Solution { // YAA
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return backtrack(nums, target, 0, 0);
-    }
-private:
-    // {(index, total) -> # of ways}
-    map<pair<int, int>, int> dp;
     
-    int backtrack(vector<int>& nums, int target, int i, int total) {
-        if (i == nums.size()) {
-            return total == target ? 1 : 0;
+    int solve(vector<int>& nums, int target, int idx, unordered_map<string, int> &dp) {
+        if(idx == nums.size()) {
+            if(target == 0) {
+                return 1;
+            }
+            return 0;
         }
-        if (dp.find({i, total}) != dp.end()) {
-            return dp[{i, total}];
+        string key = to_string(idx) + " " + to_string(target);
+        if(dp.find(key) != dp.end()) {
+            return dp[key];
         }
-        
-        dp[{i, total}] = backtrack(nums, target, i + 1, total + nums[i])
-                       + backtrack(nums, target, i + 1, total - nums[i]);
-        
-        return dp[{i, total}];
+        // +
+        int x = solve(nums, target - nums[idx], idx+1, dp);
+        // -
+        int y = solve(nums, target + nums[idx], idx+1, dp);
+        // sum
+        return dp[key] = x+y;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<string, int> dp;
+        return solve(nums, target, 0, dp);
     }
 };
