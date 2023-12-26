8dfdd6815589535b6428986317c36502192dfe3d
devanshubisht <devanshubisht.2000@gmail.com>
2023-12-26 17:25:18 +0800

    Time: 27 ms (47.77%) | Memory: 10.1 MB (25.87%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    const int mod = (int)pow(10, 9) + 7; 
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> vec(target + 1, vector<int>(n + 1, -1));
+
+        return recurse(n, k ,target, vec);
+    }
+
+    int recurse(int n , int k , int target, vector<vector<int>>& dp) {
+        if (target == 0 && n == 0) {
+            return 1;
+        }
+        if (target <= 0 || n <= 0) {
+            return 0;
+        }
+
+        if (dp[target][n] != -1) {
+            return dp[target][n];
+        }
+
+        int ways = 0;
+        for (int i = 1 ; i <= k ; i++) {
+            ways = (ways + (recurse(n - 1, k, target - i, dp)) % mod ) % mod; 
+        }
+        dp[target][n] = ways;
+        return ways;
+    }
+};
