9e5ebf29c0cc8ce4f63b48395a3de8f3ef80982c
Devanshu <devanshubisht.2000@gmail.com>
2024-04-10 13:34:53 +0800

    Time: 2 ms (91.97%) | Memory: 8.4 MB (95.49%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp (coins.size() + 1, vector<int>(amount + 1, 0));
-        for (int i = 0 ; i <= amount ; i++) {
-            dp[coins.size()][i] = 0;
-        }
-        for (int i = 0 ; i < dp.size(); i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = coins.size() - 1 ; i >= 0 ; i--) {
-            for (int j = 1 ; j <= amount ; j++) {
-                if (coins[i] <= j) {
-                    dp[i][j] = dp[i][j - coins[i]] + dp[i+1][j];
-                } else {
-                    dp[i][j] = dp[i + 1][j];
-                }
+        vector<int> dp(amount + 1);
+        dp[0] = 1; // Base case: there is one way to make change for amount 0 (using no coins)
 
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
             }
         }
-        return dp[0][amount];
-    }
 
+        return dp[amount];
+    }
 };
